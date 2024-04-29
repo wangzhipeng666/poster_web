1995969c764dfa99844d1aa59961440985d1e4b5
wangzhipeng <1848993253@qq.com>
2024-04-29 17:12:33 +0800

    删除代码

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="app-container">
-    <uploader action="http://123.57.138.48/api/upload/" />
     <router-view />
   </div>
 </template>
 
-<script lang="ts" setup>
-import Uploader from "./components/Uploader.vue";
-</script>
+<script lang="ts" setup></script>
 
 <style lang="scss"></style>
